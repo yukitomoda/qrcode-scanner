e1d26b0f48491ba1a3d01d09fc5de43f7830ef91
tomoda <ssqmuaalrle@gmail.com>
2023-09-24 15:59:44 +0900

    baseUrlをgh-pagesにあわせ修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/qrcode-scanner/'
+  },
   devtools: { enabled: true },
   ssr: false,
 })
